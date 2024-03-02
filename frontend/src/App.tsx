a6cf0a0c31b576eadd8e516b69a9f2fb5eecb5f1
Marcell <marcell.dechant@proton.me>
2024-03-02 17:36:47 +0100

    feat: Add basic layout & styling with header, main & footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import Layout from "./components/Layout.tsx";
+
 export default function App() {
     return (
-        <>
+        <Layout>
             <h1>Hello World</h1>
-        </>
+        </Layout>
     )
 }
