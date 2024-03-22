1f7e897688676486c2265c414ab1c2702825c49c
Marcell <marcell.dechant@proton.me>
2024-03-22 22:03:23 +0100

    chore: Add get all endpoint to fetch events with id & title

--- backend/src/main/java/com/github/moinmarcell/backend/event/EventRepository.java
+++ backend/src/main/java/com/github/moinmarcell/backend/event/EventRepository.java
@@ -1,8 +1,13 @@
 package com.github.moinmarcell.backend.event;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EventRepository extends MongoRepository<Event, String> {
+    @Query(value = "{}", fields = "{id: 1, title: 1}")
+    List<EventWithIdAndTitle> findAllByIdAndTitle();
 }
